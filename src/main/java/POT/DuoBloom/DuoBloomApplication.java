71fc451beb8cb2f010469de6384c568129bef2e0
toni <leesoeun2746@dgu.ac.kr>
2024-10-13 23:01:21 +0900

    🎉 init

---
+++ src/main/java/POT/DuoBloom/DuoBloomApplication.java
@@ -0,0 +1,13 @@
+package POT.DuoBloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuoBloomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuoBloomApplication.class, args);
+	}
+
+}
