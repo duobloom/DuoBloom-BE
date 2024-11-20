75c215b7fdd8c1e05b89b33d7ff5c43d83d60752
toni (이소은) <144209738+saokiritoni@users.noreply.github.com>
2024-11-20 14:19:37 +0900

    Refactor: community like n+1

--- src/main/java/POT/DuoBloom/domain/community/repository/CommunityLikeRepository.java
+++ src/main/java/POT/DuoBloom/domain/community/repository/CommunityLikeRepository.java
@@ -3,11 +3,25 @@ package POT.DuoBloom.domain.community.repository;
 import POT.DuoBloom.domain.community.entity.Community;
 import POT.DuoBloom.domain.community.entity.CommunityLike;
 import POT.DuoBloom.domain.user.entity.User;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface CommunityLikeRepository extends JpaRepository<CommunityLike, Long> {
-    Optional<CommunityLike> findByUserAndCommunity(User user, Community community);
-    long countByCommunity(Community community);
+    //Optional<CommunityLike> findByUserAndCommunity(User user, Community community);
+    @Query("SELECT cl FROM CommunityLike cl " +
+            "JOIN FETCH cl.user u " +
+            "JOIN FETCH cl.community c " +
+            "WHERE u = :user AND c = :community")
+    Optional<CommunityLike> findByUserAndCommunity(
+            @Param("user") User user,
+            @Param("community") Community community
+    );
+
+    //long countByCommunity(Community community);
+    @Query("SELECT COUNT(cl) FROM CommunityLike cl WHERE cl.community = :community")
+    long countByCommunity(@Param("community") Community community);
+
 }
