1864e06de2c1ba0f45fa3cea5f467e078516c80f
toni (이소은) <144209738+saokiritoni@users.noreply.github.com>
2024-11-20 14:24:16 +0900

    Refactor: hospital scrap n+1

--- src/main/java/POT/DuoBloom/domain/hospital/repository/HospitalScrapRepository.java
+++ src/main/java/POT/DuoBloom/domain/hospital/repository/HospitalScrapRepository.java
@@ -1,7 +1,9 @@
 package POT.DuoBloom.domain.hospital.repository;
 
 import POT.DuoBloom.domain.hospital.entity.HospitalScrap;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,10 +11,25 @@ import java.util.Optional;
 
 @Repository
 public interface HospitalScrapRepository extends JpaRepository<HospitalScrap, Long> {
-
     boolean existsByHospital_HospitalIdAndUser_UserId(Integer hospitalId, Long userId);
-    List<HospitalScrap> findByUser_UserId(Long userId);
-    Optional<HospitalScrap> findByHospital_HospitalIdAndUser_UserId(Integer hospitalId, Long userId);
+
+    //List<HospitalScrap> findByUser_UserId(Long userId);
+    //Optional<HospitalScrap> findByHospital_HospitalIdAndUser_UserId(Integer hospitalId, Long userId);
+
+    @Query("SELECT hs FROM HospitalScrap hs " +
+            "JOIN FETCH hs.hospital h " +
+            "JOIN FETCH hs.user u " +
+            "WHERE u.userId = :userId")
+    List<HospitalScrap> findByUser_UserId(@Param("userId") Long userId);
+
+    @Query("SELECT hs FROM HospitalScrap hs " +
+            "JOIN FETCH hs.hospital h " +
+            "JOIN FETCH hs.user u " +
+            "WHERE h.hospitalId = :hospitalId AND u.userId = :userId")
+    Optional<HospitalScrap> findByHospital_HospitalIdAndUser_UserId(
+            @Param("hospitalId") Integer hospitalId,
+            @Param("userId") Long userId
+    );
 }
 
 
