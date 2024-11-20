a2a9563055aadcea1cff2fa450532832ab78a4c0
toni (이소은) <144209738+saokiritoni@users.noreply.github.com>
2024-11-20 14:09:29 +0900

    Refactor: board scrap n+1

--- src/main/java/POT/DuoBloom/domain/board/repository/BoardScrapRepository.java
+++ src/main/java/POT/DuoBloom/domain/board/repository/BoardScrapRepository.java
@@ -1,7 +1,9 @@
 package POT.DuoBloom.domain.board.repository;
 
 import POT.DuoBloom.domain.board.entity.BoardScrap;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +14,21 @@ public interface BoardScrapRepository extends JpaRepository<BoardScrap, Long> {
 
     boolean existsByBoard_BoardIdAndUser_UserId(Integer boardId, Long userId);
 
-    List<BoardScrap> findByUser_UserId(Long userId);
-    Optional<BoardScrap> findByBoard_BoardIdAndUser_UserId(Integer boardId, Long userId);
+    //List<BoardScrap> findByUser_UserId(Long userId);
+    //Optional<BoardScrap> findByBoard_BoardIdAndUser_UserId(Integer boardId, Long userId);
+
+    @Query("SELECT bs FROM BoardScrap bs " +
+            "JOIN FETCH bs.board b " +
+            "JOIN FETCH bs.user u " +
+            "WHERE u.userId = :userId")
+    List<BoardScrap> findByUser_UserId(@Param("userId") Long userId);
+
+    @Query("SELECT bs FROM BoardScrap bs " +
+            "JOIN FETCH bs.board b " +
+            "JOIN FETCH bs.user u " +
+            "WHERE b.boardId = :boardId AND u.userId = :userId")
+    Optional<BoardScrap> findByBoard_BoardIdAndUser_UserId(
+            @Param("boardId") Integer boardId,
+            @Param("userId") Long userId
+    );
 }
