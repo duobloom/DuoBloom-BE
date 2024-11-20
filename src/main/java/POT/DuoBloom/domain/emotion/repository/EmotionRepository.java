50f50d321367ff4f0fe1f6d07f9635c19ca3fc14
toni (이소은) <144209738+saokiritoni@users.noreply.github.com>
2024-11-20 14:21:44 +0900

    Refactor: emotion n+1

--- src/main/java/POT/DuoBloom/domain/emotion/repository/EmotionRepository.java
+++ src/main/java/POT/DuoBloom/domain/emotion/repository/EmotionRepository.java
@@ -2,7 +2,9 @@ package POT.DuoBloom.domain.emotion.repository;
 
 import POT.DuoBloom.domain.emotion.entity.Emotion;
 import POT.DuoBloom.domain.user.entity.User;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -10,5 +12,13 @@ import java.util.List;
 
 @Repository
 public interface EmotionRepository extends JpaRepository<Emotion, Long> {
-    List<Emotion> findByFeedDateAndUser(LocalDate feedDate, User user);
+    //List<Emotion> findByFeedDateAndUser(LocalDate feedDate, User user);
+
+    @Query("SELECT e FROM Emotion e " +
+            "JOIN FETCH e.user u " +
+            "WHERE e.feedDate = :feedDate AND u = :user")
+    List<Emotion> findByFeedDateAndUser(
+            @Param("feedDate") LocalDate feedDate,
+            @Param("user") User user
+    );
 }
