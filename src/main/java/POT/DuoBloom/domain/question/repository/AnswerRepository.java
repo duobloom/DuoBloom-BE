452354a80fa693166fd97f75e63de3c96cdd6e5e
toni (이소은) <144209738+saokiritoni@users.noreply.github.com>
2024-11-20 14:29:43 +0900

    Refactor: answer question n+1

--- src/main/java/POT/DuoBloom/domain/question/repository/AnswerRepository.java
+++ src/main/java/POT/DuoBloom/domain/question/repository/AnswerRepository.java
@@ -1,18 +1,27 @@
 package POT.DuoBloom.domain.question.repository;
 
 import POT.DuoBloom.domain.question.entity.Answer;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
 
-
     // 특정 질문 ID와 사용자 ID로 답변 조회
-    List<Answer> findByQuestion_QuestionIdAndUser_UserId(Long questionId, Long userId);
+    //List<Answer> findByQuestion_QuestionIdAndUser_UserId(Long questionId, Long userId);
+
+    @Query("SELECT a FROM Answer a " +
+            "JOIN FETCH a.question q " +
+            "JOIN FETCH a.user u " +
+            "WHERE q.questionId = :questionId AND u.userId = :userId")
+    List<Answer> findByQuestion_QuestionIdAndUser_UserId(
+            @Param("questionId") Long questionId,
+            @Param("userId") Long userId
+    );
 
 
 
--- src/main/java/POT/DuoBloom/domain/question/repository/QuestionRepository.java
+++ src/main/java/POT/DuoBloom/domain/question/repository/QuestionRepository.java
@@ -1,15 +1,20 @@
 package POT.DuoBloom.domain.question.repository;
 
 import POT.DuoBloom.domain.question.entity.Question;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    List<Question> findAllByFeedDate(LocalDate feedDate);
+    //List<Question> findAllByFeedDate(LocalDate feedDate);
+    @Query("SELECT q FROM Question q " +
+            "LEFT JOIN FETCH q.answers a " +
+            "WHERE q.feedDate = :feedDate")
+    List<Question> findAllByFeedDate(@Param("feedDate") LocalDate feedDate);
 
 }
